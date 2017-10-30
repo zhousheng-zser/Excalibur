89ac4b4741daabd20fa3637468126a1fce740cbb
Usami Saori <Accelouch@gmail.com>
2017-10-30 21:31:15 +0800

    add mirrormax operation to replace slice

---
+++ Excalibur/Excalibur/mirrormax.cu
@@ -0,0 +1,12 @@
+#include "mirrormax.hpp"
+#ifdef USE_CUDA
+
+namespace excalibur
+{
+	void mirrormax::Forward_native_gpu(const std::shared_ptr<tensor>& bottom, std::shared_ptr<tensor>& top)
+	{
+
+	}
+}
+
+#endif
