e3f1bad7fc2dc858a32e74caf5fb328eda21a95f
Usami Saori <Accelouch@gmail.com>
2017-11-05 21:35:11 +0800

    rearrange flaskcv

---
+++ Excalibur/FlaskCV/graphicoperations.hpp
@@ -0,0 +1,21 @@
+#pragma once
+#ifndef _GRAPHICOPERATIONS_HPP_
+#define _GRAPHICOPERATIONS_HPP_
+
+#include "graphic.hpp"
+
+namespace flaskcv
+{
+	class graphicoperations
+	{
+		static void bilinear_resize_cpu(std::shared_ptr<graphic> src, std::shared_ptr<graphic>& dst, int new_height, int new_width);
+
+		static void flip_cpu(std::shared_ptr<graphic> src, std::shared_ptr<graphic>& dst, bool flip_height, bool flip_width);
+	public:
+		graphicoperations();
+		~graphicoperations();
+	};
+}
+
+
+#endif _GRAPHICOPERATIONS_HPP_
