69751017c47a2bef97b14d63ee9bbacb6b33fbe6
Usami Saori <Accelouch@gmail.com>
2017-12-06 15:18:42 +0800

    rearrange IPBbox& IPTs logical
    one cuBLAS_handle was used for 2 nets to save memory

--- Excalibur/Longinus/alcnn.cpp
+++ Excalibur/Longinus/alcnn.cpp
@@ -37,112 +37,35 @@ namespace glasssix
 		delete ipts;
 	}
 
-#ifdef USE_OPENCV
-	cv::Mat alcnn::safetycut(cv::Mat ori, cv::Rect rect)
+	void alcnn::Forward_IPBbox(const std::shared_ptr<tensor> input_data)
 	{
-		cv::Mat mat(rect.height, rect.width, ori.type(), cv::Scalar(0, 0, 0));
-		if (rect.x >= 0 && rect.y >= 0 && (rect.x + rect.width) <= ori.cols && (rect.y + rect.height) <= ori.rows)
+		if (device_>=0)
 		{
-			ori(rect).copyTo(mat);
-		}
-		else
-		{
-			cv::Mat border_mat;
-			int x_scale = std::max(std::max(-1 * rect.x, rect.x + rect.width - ori.cols), -1) + 1;
-			int y_scale = std::max(std::max(-1 * rect.y, rect.y + rect.height - ori.rows), -1) + 1;
-			cv::copyMakeBorder(ori, border_mat, y_scale, y_scale, x_scale, x_scale, cv::BORDER_CONSTANT);
-			rect.x += x_scale;
-			rect.y += y_scale;
-			border_mat(rect).copyTo(mat);
-		}
-		return mat;
-	}
-
-	void alcnn::alignface_opencv(cv::Mat& img, cv::Mat& aligned)
-	{
-		cv::Mat tempMat;
-		cv::resize(img, tempMat, cv::Size(60, 60), cv::INTER_CUBIC);
-		std::shared_ptr<tensor> mat_tensor = nullptr;
-		io::image2tensor(tempMat, mat_tensor);
-		if (device_<0)
-			ipbbox->Forward_cpu(mat_tensor);
-		else
-			ipbbox->Forward_native_gpu(mat_tensor, cublas_handle_);
-		auto bbox_data = ipbbox->get_fc2()->cpu_data();
-		auto headpose_data = ipbbox->get_fc3()->cpu_data();
-
-		cv::Rect face_rect((int)(bbox_data[0] * img.cols), (int)(bbox_data[1] * img.rows), (int)((bbox_data[2] - bbox_data[0]) * img.cols), (int)((bbox_data[3] - bbox_data[1]) * img.rows));
-		cv::Point2f center((bbox_data[0] + bbox_data[2]) * img.cols / 2, (bbox_data[1] + bbox_data[3]) * img.rows / 2);
-		double angletheta = headpose_data[2] * 90;
-		double arctheta = angletheta * CV_PI / 180;
-		cv::Mat rot_mat = cv::getRotationMatrix2D(center, -1 * angletheta, 1.0);
-		cv::Mat roted_img;
-		cv::warpAffine(img, roted_img, rot_mat, img.size(), cv::INTER_CUBIC, cv::BORDER_CONSTANT, cv::Scalar::all(0));
-
-		int delta = (int)(sin(arctheta) * face_rect.height / 2);
-		face_rect.x += delta;
-		center.x += delta;
-		int delta_pitch = (int)((1 - cos((double)headpose_data[1] * 90 * CV_PI / 180))*face_rect.height / 2);
-
-		face_rect.y -= delta_pitch;
-		face_rect.height += 2 * delta_pitch;
-
-		int margin_height = (int)(face_rect.height * 1.2);
-		int margin_x = (int)(center.x - margin_height / 2);
-		int margin_y = (int)(center.y - margin_height / 2);
-
-		cv::Rect margin_rect(margin_x, margin_y, margin_height, margin_height);
-		cv::Mat C = safetycut(roted_img, margin_rect);
-		//5IPTs
-		cv::resize(C, tempMat, cv::Size(60, 60), cv::INTER_CUBIC);
-		io::image2tensor(tempMat, mat_tensor);
-		if (device_<0)
-			ipts->Forward_cpu(mat_tensor);
-		else
-			ipts->Forward_native_gpu(mat_tensor, cublas_handle_);
-		auto lmdk_data = ipts->get_fc2()->cpu_data();
-		cv::Point2f center_eye((lmdk_data[0] + lmdk_data[2]) / 2 * C.cols, (lmdk_data[1] + lmdk_data[3]) / 2 * C.rows);
-		cv::Point2f center_mouth((lmdk_data[6] + lmdk_data[8]) / 2 * C.cols, (lmdk_data[7] + lmdk_data[9]) / 2 * C.rows);
-		cv::Point2f half_square(0.5f * C.cols, 0.25f * C.rows);
-
-		float distance_x = center_eye.x - half_square.x;
-		float distance_y = center_eye.y - half_square.y;
-		float distance_me = (float)sqrt((center_mouth.x - center_eye.x) * (center_mouth.x - center_eye.x) + (center_mouth.y - center_eye.y) * (center_mouth.y - center_eye.y));
-		float scale = distance_me / (C.rows * 0.5f);
-
-		margin_rect.x += (int)distance_x;
-		margin_rect.y += (int)distance_y;
-
-		double tan = (lmdk_data[1] - lmdk_data[3]) / (lmdk_data[0] + lmdk_data[2]);
-		double arctan = atan(tan) * 180 / CV_PI;
-
-		cv::Mat rot_mat_2 = cv::getRotationMatrix2D(cv::Point2f(half_square.x + margin_rect.x, half_square.y + margin_rect.y), -3 * arctan, 1.0);
-		cv::Mat roted_img_2;
-		cv::warpAffine(roted_img, roted_img_2, rot_mat_2, roted_img.size(), cv::INTER_CUBIC, cv::BORDER_CONSTANT, cv::Scalar::all(0));
-
-		margin_rect.x = margin_rect.x + (int)((1 - scale) * margin_rect.width * 0.5f);
-		margin_rect.y = margin_rect.y + (int)((1 - scale) * margin_rect.height * 0.25f);
-		margin_rect.width = (int)(margin_rect.width * scale);
-		margin_rect.height = (int)(margin_rect.height * scale);
-		cv::Mat F = safetycut(roted_img_2, margin_rect);
-		cv::Rect final_rect((int)(0.0f / 14 * F.cols), 0, (int)(14.0f / 14 * F.cols), F.rows);
-		cv::Mat Gray(final_rect.height, final_rect.width, F.type(), cv::Scalar::all(0));
-		cv::cvtColor(F(final_rect), Gray, CV_BGR2GRAY);
-		cv::equalizeHist(Gray, Gray);
-		cv::Mat colorimg;
-		cv::merge(std::vector<cv::Mat>{ Gray, Gray, Gray }, colorimg);
-		cv::resize(colorimg, aligned, cv::Size(128, 128), cv::INTER_CUBIC);
-	}
-
-	std::shared_ptr<tensor> alcnn::alignface(cv::Mat& img)
-	{
-		std::shared_ptr<tensor> mat_tensor = nullptr;
-		cv::Mat aligned;
-		alignface_opencv(img, aligned);
-		io::image2tensor(aligned, mat_tensor);
-		return mat_tensor;
-	}
-
+#ifdef USE_CUDA
+			ipbbox->Forward_native_gpu(input_data, cublas_handle_);
+#else
+			NO_GPU;
 #endif
+		}
+		else
+		{
+			ipbbox->Forward_cpu(input_data);
+		}
+	}
 
+	void alcnn::Forward_IPTs(const std::shared_ptr<tensor> input_data)
+	{
+		if (device_ >= 0)
+		{
+#ifdef USE_CUDA
+			ipts->Forward_native_gpu(input_data, cublas_handle_);
+#else
+			NO_GPU;
+#endif
+		}
+		else
+		{
+			ipts->Forward_cpu(input_data);
+		}
+	}
 }
--- Excalibur/Longinus/longinus.cpp
+++ Excalibur/Longinus/longinus.cpp
@@ -1,6 +1,5 @@
 #include <msclr\marshal_cppstd.h>
-#include "ipbbox_net.hpp"
-#include "ipts_net.hpp"
+#include "alcnn.hpp"
 
 using namespace System;
 using namespace System::Runtime::InteropServices;
@@ -16,48 +15,49 @@ using namespace System::Drawing::Imaging;
 
 namespace glasssix
 {
-	public ref class ipbbox
+
+	public ref class longinus
 	{
 	private:
 		int device_;
-		ipbbox_net* ipbbox_net_;
+		alcnn* alcnn_net_;
 	public:
-		ipbbox(int device)
+		longinus(int device)
 		{
 			device_ = device;
-			ipbbox_net_ = new ipbbox_net(device_);
+			alcnn_net_ = new alcnn(device_);
 		}
 
-		!ipbbox()
+		!longinus()
 		{
-			delete ipbbox_net_;
-			ipbbox_net_ = nullptr;
+			delete alcnn_net_;
+			alcnn_net_ = nullptr;
 		}
 
-		~ipbbox()
+		~longinus()
 		{
-			this->!ipbbox();
+			this->!longinus();
 		}
 
-		array<array<float>^>^ ExtractBitmapOutputs(array<Bitmap^>^ imgDatas, int DeviceId)
+		array<array<float>^>^ ExtractBitmapOutputs_IPBbox(array<Bitmap^>^ imgDatas)
 		{
 			std::shared_ptr<tensor> tensor_data = nullptr;
 			bitmaps2tensor(imgDatas, tensor_data);
-			ipbbox_net_->Forward(tensor_data);
+			alcnn_net_->Forward_IPBbox(tensor_data);
 			auto outputs = gcnew array<array<float>^>(static_cast<int>(2));
 			for (int i = 0; i < outputs->Length; i++)
 			{
-				if (i==0)
+				if (i == 0)
 				{
-					const float* intermediate = ipbbox_net_->get_fc2()->cpu_data();
-					int output_size = ipbbox_net_->get_fc2()->count();
+					const float* intermediate = alcnn_net_->get_IPBbox_fc2_data();
+					int output_size = alcnn_net_->get_IPBbox_fc2_count();
 					MARSHAL_ARRAY(intermediate, output, output_size);
 					outputs[i] = output;
 				}
-				if (i==1)
+				if (i == 1)
 				{
-					const float* intermediate = ipbbox_net_->get_fc3()->cpu_data();
-					int output_size = ipbbox_net_->get_fc3()->count();
+					const float* intermediate = alcnn_net_->get_IPBbox_fc3_data();
+					int output_size = alcnn_net_->get_IPBbox_fc2_count();
 					MARSHAL_ARRAY(intermediate, output, output_size);
 					outputs[i] = output;
 				}
@@ -65,90 +65,13 @@ namespace glasssix
 			return outputs;
 		}
 
-	private:
-		static void bitmaps2tensor(array<Bitmap^>^ bitmaps, std::shared_ptr<tensor>& tensor_data)
-		{
-			int num = bitmaps->Length;
-			if (num <= 0)
-			{
-				return;
-			}
-			int channel = ipbbox_net::get_input_channel();
-			float mean[] = { 104.0f, 117.0f, 124.0f };
-			float scale = 0.0078125f;
-			int width = ipbbox_net::get_input_width();
-			int height = ipbbox_net::get_input_height();
-			int n_offset = channel * height * width;
-			int c_offset = height * width;
-
-			Drawing::Rectangle rc = Drawing::Rectangle(0, 0, width, height);
-			tensor_data.reset(new tensor(std::vector<int>{num, channel, height, width}, -1));
-			float* float_data = tensor_data->mutable_cpu_data();
-
-			for (int n = 0; n < num; n++)
-			{
-				Bitmap^ resize_bitmap;
-				if (width == bitmaps[n]->Width && height == bitmaps[n]->Height)
-				{
-					// no other situations, only channel == 3 for unicorn net
-					resize_bitmap = bitmaps[n]->Clone(rc, PixelFormat::Format24bppRgb);
-				}
-				else
-				{
-					resize_bitmap = gcnew Bitmap((Image ^)bitmaps[n], width, height);
-					resize_bitmap = resize_bitmap->Clone(rc, PixelFormat::Format24bppRgb);
-				}
-				// get image data block
-				BitmapData ^bmpData = resize_bitmap->LockBits(rc, ImageLockMode::ReadOnly, resize_bitmap->PixelFormat);
-				pin_ptr<unsigned char> bmpBuffer = (unsigned char *)bmpData->Scan0.ToPointer();
-
-				for (int c = 0; c < channel; ++c)
-				{
-					for (int h = 0; h < height; ++h)
-					{
-						int line_offset = h * bmpData->Stride + c;
-						for (int w = 0; w < width; ++w)
-						{
-							float_data[n*n_offset + c*c_offset + h*width + w] =
-								(static_cast<float>(bmpBuffer[line_offset + w * channel]) - mean[c]) * scale;
-						}
-					}
-				}
-				resize_bitmap->UnlockBits(bmpData);
-			}
-		}
-	};
-
-	public ref class ipts
-	{
-	private:
-		int device_;
-		ipts_net* ipts_net_;
-	public:
-		ipts(int device)
-		{
-			device_ = device;
-			ipts_net_ = new ipts_net(device_);
-		}
-
-		!ipts()
-		{
-			delete ipts_net_;
-			ipts_net_ = nullptr;
-		}
-
-		~ipts()
-		{
-			this->!ipts();
-		}
-
 		array<float>^ ExtractBitmapOutputs(array<Bitmap^>^ imgDatas, int DeviceId)
 		{
 			std::shared_ptr<tensor> tensor_data = nullptr;
 			bitmaps2tensor(imgDatas, tensor_data);
-			ipts_net_->Forward(tensor_data);
-			const float* intermediate = ipts_net_->get_fc2()->cpu_data();
-			int output_size = ipts_net_->get_fc2()->count();
+			alcnn_net_->Forward_IPTs(tensor_data);
+			const float* intermediate = alcnn_net_->get_IPTs_fc2_data();
+			int output_size = alcnn_net_->get_IPTs_fc2_count();
 			MARSHAL_ARRAY(intermediate, outputs, output_size);
 			return outputs;
 		}
@@ -161,11 +84,11 @@ namespace glasssix
 			{
 				return;
 			}
-			int channel = ipbbox_net::get_input_channel();
+			int channel = 3;// ipbbox_net::get_input_channel();
 			float mean[] = { 104.0f, 117.0f, 124.0f };
 			float scale = 0.0078125f;
-			int width = ipbbox_net::get_input_width();
-			int height = ipbbox_net::get_input_height();
+			int width = 60;// ipbbox_net::get_input_width();
+			int height = 60;// ipbbox_net::get_input_height();
 			int n_offset = channel * height * width;
 			int c_offset = height * width;
 
@@ -205,6 +128,5 @@ namespace glasssix
 				resize_bitmap->UnlockBits(bmpData);
 			}
 		}
-
 	};
 }
