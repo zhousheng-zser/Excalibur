f586d91c553861017ae590079ce0c28fee73881b
fengye2two <fengye2two@163.com>
2019-02-26 09:29:19 +0800

    julius reorganize

--- Excalibur/Julius/julius_gemm.hpp
+++ Excalibur/Julius/julius_gemm.hpp
@@ -1,6 +1,7 @@
 #pragma once
 #ifndef _JULIUS_GEMM_HPP_
 #define _JULIUS_GEMM_HPP_
+#include "simd_helper.hpp"
 
 namespace glasssix
 {
@@ -8,105 +9,17 @@ namespace glasssix
 	{
 		namespace juliusblas
 		{
-			inline void cblas_sgemm_AnoTrans_BnoTrans(const int M, const int N, const int K, const float alpha, const float* A, const int lda,
-				const float* B, const int ldb, const float beta, float* C, const int ldc)
-			{
-#if SIDM_TYPE >= SIMDTYPE_AVX512
-				NOT_IMPLEMENTED;
-#elif SIMD_TYPE >= SIMDTYPE_AVX
-				NOT_IMPLEMENTED;
-#elif SIMD_TYPE >= SIMDTYPE_SSE
-				NOT_IMPLEMENTED;
-#else SIMD_TYPE == SIMDTYPE_NONE
-				// Fall back to native code
-				for (size_t i = 0; i < N; i++)
-				{
-					for (size_t j = 0; j < M; j++)
-					{
-						C[j * ldc + i] = beta * C[j * ldc + i];
-						for (size_t k = 0; k < K; k++)
-						{
-							C[j * ldc + i] += alpha * A[j * lda + k] * B[k * ldb + i];
-						}
-					}
-				}
-#endif 
-			}
+			void cblas_sgemm_AnoTrans_BnoTrans(const int M, const int N, const int K, const float alpha, const float* A, const int lda,
+				const float* B, const int ldb, const float beta, float* C, const int ldc);
 
-			inline void cblas_sgemm_ATrans_BnoTrans(const int M, const int N, const int K, const float alpha, const float* A, const int lda,
-				const float* B, const int ldb, const float beta, float* C, const int ldc)
-			{
-#if SIDM_TYPE >= SIMDTYPE_AVX512
-				NOT_IMPLEMENTED;
-#elif SIMD_TYPE >= SIMDTYPE_AVX
-				NOT_IMPLEMENTED;
-#elif SIMD_TYPE >= SIMDTYPE_SSE
-				NOT_IMPLEMENTED;
-#else SIMD_TYPE == SIMDTYPE_NONE
-				// Fall back to native code
-				for (size_t i = 0; i < N; i++)
-				{
-					for (size_t j = 0; j < M; j++)
-					{
-						C[j * ldc + i] = beta * C[j * ldc + i];
-						for (size_t k = 0; k < K; k++)
-						{
-							C[j * ldc + i] += alpha * A[k * lda + j] * B[k * ldb + i];
-						}
-					}
-				}
-#endif 
-			}
+			void cblas_sgemm_ATrans_BnoTrans(const int M, const int N, const int K, const float alpha, const float* A, const int lda,
+				const float* B, const int ldb, const float beta, float* C, const int ldc);
 
-			inline void cblas_sgemm_AnoTrans_BTrans(const int M, const int N, const int K, const float alpha, const float* A, const int lda,
-				const float* B, const int ldb, const float beta, float* C, const int ldc)
-			{
-#if SIDM_TYPE >= SIMDTYPE_AVX512
-				NOT_IMPLEMENTED;
-#elif SIMD_TYPE >= SIMDTYPE_AVX
-				NOT_IMPLEMENTED;
-#elif SIMD_TYPE >= SIMDTYPE_SSE
-				NOT_IMPLEMENTED;
-#else SIMD_TYPE == SIMDTYPE_NONE
-				// Fall back to native code
-				for (size_t i = 0; i < N; i++)
-				{
-					for (size_t j = 0; j < M; j++)
-					{
-						C[j * ldc + i] = beta * C[j * ldc + i];
-						for (size_t k = 0; k < K; k++)
-						{
-							C[j * ldc + i] += alpha * A[j * lda + k] * B[i * ldb + k];
-						}
-					}
-				}
-#endif 
-			}
+			void cblas_sgemm_AnoTrans_BTrans(const int M, const int N, const int K, const float alpha, const float* A, const int lda,
+				const float* B, const int ldb, const float beta, float* C, const int ldc);
 
-			inline void cblas_sgemm_ATrans_BTrans(const int M, const int N, const int K, const float alpha, const float* A, const int lda,
-				const float* B, const int ldb, const float beta, float* C, const int ldc)
-			{
-#if SIDM_TYPE >= SIMDTYPE_AVX512
-				NOT_IMPLEMENTED;
-#elif SIMD_TYPE >= SIMDTYPE_AVX
-				NOT_IMPLEMENTED;
-#elif SIMD_TYPE >= SIMDTYPE_SSE
-				NOT_IMPLEMENTED;
-#else SIMD_TYPE == SIMDTYPE_NONE
-				// Fall back to native code
-				for (size_t i = 0; i < N; i++)
-				{
-					for (size_t j = 0; j < M; j++)
-					{
-						C[j * ldc + i] = beta * C[j * ldc + i];
-						for (size_t k = 0; k < K; k++)
-						{
-							C[j * ldc + i] += alpha * A[k * lda + j] * B[i * ldb + k];
-						}
-					}
-				}
-#endif 
-			}
+			void cblas_sgemm_ATrans_BTrans(const int M, const int N, const int K, const float alpha, const float* A, const int lda,
+				const float* B, const int ldb, const float beta, float* C, const int ldc);
 		};
 	}
 }
