602c9baaad7aab9a6d2aaf3f21c7e9f265e81668
Usami Saori <inlmouse@hotmail.com>
2019-03-04 18:37:56 +0800

    Add: Cassiunia and Longinucia support in C++/CLI

--- include/Longinus/ImageOperation.hpp
+++ include/Longinus/ImageOperation.hpp
@@ -7,21 +7,21 @@ namespace glasssix
 {
 	namespace longinus
 	{
-		LONGINUS_DLL void myResize(const unsigned char * psrc, int swidth, int sheight, int sstep,
+		/*LONGINUS_DLL*/ void myResize(const unsigned char * psrc, int swidth, int sheight, int sstep,
 			unsigned char * pdst, int dwidth, int dheight, int dstep);
-		LONGINUS_DLL void myFlip(const unsigned char * psrc, int width, int height, int step,
+		/*LONGINUS_DLL*/ void myFlip(const unsigned char * psrc, int width, int height, int step,
 			unsigned char * pdst);
-		LONGINUS_DLL void myIntegral(const unsigned char *pSrc, int width, int height, int step, int *pSum, int sum_width);
+		/*LONGINUS_DLL*/ void myIntegral(const unsigned char *pSrc, int width, int height, int step, int *pSum, int sum_width);
 
-		LONGINUS_DLL void resize_cpu_bilinear(const unsigned char* src_data, int old_height, int old_width, int channels,
+		/*LONGINUS_DLL*/ void resize_cpu_bilinear(const unsigned char* src_data, int old_height, int old_width, int channels,
 			unsigned char* dst_data, int new_height, int new_width);
 #ifdef USE_CUDA
-		LONGINUS_DLL void resize_gpu_bilinear(const unsigned char* src_data, int old_height, int old_width, int channels,
+		/*LONGINUS_DLL*/ void resize_gpu_bilinear(const unsigned char* src_data, int old_height, int old_width, int channels,
 			unsigned char* dst_data, int new_height, int new_width, int device);
 
-		LONGINUS_DLL void integral_gpu(const unsigned char* src_data, int width, int height, int* dst_data, int sum_width, int sum_height);
+		/*LONGINUS_DLL*/ void integral_gpu(const unsigned char* src_data, int width, int height, int* dst_data, int sum_width, int sum_height);
 #endif
-		LONGINUS_DLL void matrix_transpose(const unsigned char *src_data, int height, int width, unsigned char *dst_data);
+		/*LONGINUS_DLL*/ void matrix_transpose(const unsigned char *src_data, int height, int width, unsigned char *dst_data);
 	}
 }
 
