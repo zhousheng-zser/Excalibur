bfc71518d9a630c68ec016bb02224b136dbf04c9
fengye2two <fengye2two@163.com>
2019-03-04 10:35:31 +0800

    split convolution

---
+++ Excalibur/Excalibur/conv_native_cpu.cpp
@@ -0,0 +1,162 @@
+#include "conv_native_cpu.hpp"
+
+namespace glasssix
+{
+	namespace excalibur
+	{
+		void conv_native_cpu::forward_gemm(const float* input, const float* weights, float* output, bool skip_im2col)
+		{
+			const float* col_buff = input;
+
+			if ((kernelSize_ != 1) || (order_ == NHWC))
+			{
+				conv_im2col_cpu(input, col_buffer_->mutable_cpu_data());
+				col_buff = col_buffer_->cpu_data();
+			}
+
+			if (order_ == NCHW)
+			{
+				if (group_ == 1)
+				{
+					math_functions::cpu_sgemm(CblasNoTrans, CblasNoTrans, output_Channel_,
+						output_spatial_dim_, kernel_dim_, 1.0f,
+						weights, col_buff, 0.0f, output);
+				}
+				else
+				{
+					for (int g = 0; g < group_; ++g)
+					{
+						math_functions::cpu_sgemm(CblasNoTrans, CblasNoTrans, output_Channel_ / group_,
+							output_spatial_dim_, kernelSize_ * kernelSize_, 1.0f,
+							weights + kernelSize_ * kernelSize_ * g, col_buff + output_spatial_dim_ * kernelSize_ * kernelSize_ * g, 0.0f, output + output_spatial_dim_ * g);
+					}
+				}
+			}
+			else if (order_ == NHWC)
+			{
+				if (group_ == 1)
+				{
+					math_functions::cpu_sgemm(CblasTrans, CblasTrans, output_spatial_dim_, output_Channel_,
+						kernel_dim_, 1.0f,
+						col_buff, weights, 0.0f, output);
+				}
+				else
+				{
+					for (int g = 0; g < group_; ++g)
+					{
+						math_functions::cpu_sgemm(CblasTrans, CblasTrans, output_spatial_dim_, output_Channel_ / group_,
+							kernelSize_ * kernelSize_, 1.0f,
+							col_buff + output_spatial_dim_ * kernelSize_ * kernelSize_ * g, weights + kernelSize_ * kernelSize_ * g, 0.0f, output + output_spatial_dim_ * g);
+					}
+
+					float* temp_data = (float*)malloc(output_spatial_dim_ * output_Channel_ * sizeof(float));
+					for (int ch = 0; ch < output_Channel_; ++ch)
+					{
+						int channel_offset = ch * output_dim_h_ * output_dim_w_;
+						for (int row = 0; row < output_dim_h_; ++row)
+						{
+							int row_offset = row * output_dim_w_;
+							for (int col = 0; col < output_dim_w_; ++col)
+							{
+								temp_data[(row_offset + col) * output_Channel_ + ch] = output[channel_offset + row_offset + col];
+							}
+						}
+					}
+					memcpy(output, temp_data, output_spatial_dim_ * output_Channel_ * sizeof(float));
+					delete temp_data;
+				}
+			}
+			else
+			{
+				NOT_IMPLEMENTED;
+			}
+		}
+
+		void conv_native_cpu::forward_bias(float* output, const float* bias)
+		{
+			if (order_ == NCHW)
+			{
+				math_functions::cpu_sgemm(CblasNoTrans, CblasNoTrans, output_Channel_,
+					output_spatial_dim_, 1, 1.0f, bias, bias_multiplier_->cpu_data(),
+					1.0f, output);
+			}
+			else if (order_ == NHWC)
+			{
+				math_functions::cpu_sgemm(CblasNoTrans, CblasNoTrans, output_spatial_dim_,
+					output_Channel_, 1, 1.0f, bias_multiplier_->cpu_data(), bias,
+					1.0f, output);
+			}
+			else
+			{
+				NOT_IMPLEMENTED;
+			}
+		}
+
+		void conv_native_cpu::Forward(const std::shared_ptr<tensor<float>>& bottom, std::shared_ptr<tensor<float>>& top)
+		{
+			num_ = bottom->data_shape()[0];
+			order_ = bottom->order();
+			const float* bottom_data = bottom->cpu_data();
+			const float* weights = weights_->cpu_data();
+			const float* bias = bias_->cpu_data();
+			intput_shape_.clear();
+			intput_shape_ = bottom->data_shape();
+
+			if (order_ == NCHW)
+			{
+				output_dim_h_ = (bottom->data_shape()[2] + 2 * pad_ - kernelSize_) / stride_ + 1;
+				output_dim_w_ = (bottom->data_shape()[3] + 2 * pad_ - kernelSize_) / stride_ + 1;
+				top.reset(new tensor<float>(std::vector<int>{num_, output_Channel_, output_dim_h_, output_dim_w_}, device_, order_));
+				float* top_data = (top)->mutable_cpu_data();
+
+				col_buffer_.reset(new tensor<float>(std::vector<int>{kernel_dim_, output_dim_h_, output_dim_w_}, device_));
+				bias_multiplier_.reset(new tensor<float>(std::vector<int>{output_dim_w_*output_dim_h_}, device_));
+				output_spatial_dim_ = output_dim_w_*output_dim_h_;
+				col_offset_ = kernel_dim_ * output_spatial_dim_;
+				output_offset_ = output_Channel_ * output_spatial_dim_ / group_;
+				math_functions::cpu_set(output_dim_w_*output_dim_h_, 1.0f, bias_multiplier_->mutable_cpu_data());
+				//
+				int bottom_dim_ = bottom->data_shape()[1] * bottom->data_shape()[2] * bottom->data_shape()[3];
+				int top_dim = (top)->count(1, 4);
+				for (int n = 0; n < num_; n++)
+				{
+					forward_gemm(bottom_data + n * bottom_dim_, weights, top_data + n * top_dim);
+					if (bias_term_)
+					{
+						forward_bias(top_data + n * top_dim, bias);
+					}
+				}
+			}
+			else if (order_ == NHWC)
+			{
+				output_dim_h_ = (bottom->data_shape()[1] + 2 * pad_ - kernelSize_) / stride_ + 1;
+				output_dim_w_ = (bottom->data_shape()[2] + 2 * pad_ - kernelSize_) / stride_ + 1;
+				top.reset(new tensor<float>(std::vector<int>{num_, output_dim_h_, output_dim_w_, output_Channel_}, device_, order_));
+				float* top_data = (top)->mutable_cpu_data();
+
+				col_buffer_.reset(new tensor<float>(std::vector<int>{kernel_dim_*group_, output_dim_h_, output_dim_w_}, device_));
+				bias_multiplier_.reset(new tensor<float>(std::vector<int>{output_dim_w_*output_dim_h_}, device_));
+				output_spatial_dim_ = output_dim_w_*output_dim_h_;
+				output_spatial_dim_ = output_dim_w_*output_dim_h_;
+				col_offset_ = kernel_dim_ * output_spatial_dim_;
+				output_offset_ = output_Channel_ * output_spatial_dim_ / group_;
+				math_functions::cpu_set(output_dim_w_*output_dim_h_, 1.0f, bias_multiplier_->mutable_cpu_data());
+				//
+				int bottom_dim_ = bottom->data_shape()[1] * bottom->data_shape()[2] * bottom->data_shape()[3];
+				int top_dim = (top)->count(1, 4);
+				for (int n = 0; n < num_; n++)
+				{
+					forward_gemm(bottom_data + n * bottom_dim_, weights, top_data + n * top_dim);
+					if (bias_term_)
+					{
+						forward_bias(top_data + n * top_dim, bias);
+					}
+				}
+			}
+			else
+			{
+				NOT_IMPLEMENTED;
+			}
+		}
+	}
+}
---
+++ Excalibur/Excalibur/conv_winograd_cpu.hpp
@@ -0,0 +1,128 @@
+#ifndef _CONV_WINOGRAD_CPU_HPP_
+#define _CONV_WINOGRAD_CPU_HPP_
+#include "base_conv.hpp"
+namespace glasssix
+{
+	namespace excalibur
+	{
+		class conv_winograd_cpu : public baseconv
+		{
+		public:
+			int tile_size_;
+			int tile_length_;
+			int h_tile_num_;
+			int w_tile_num_;
+			int V_num_;//the quantity of V
+			int U_num_;//the quantity of U
+			float* U_;
+			float* V_;
+			float *tile_data, *v_data, *m_data, *result;
+
+			//winograd F(2,3)
+			int m_ = 2;
+			const float A_[8] = { 1.0f,  0.0f,
+				                  1.0f,  1.0f,
+				                  1.0f, -1.0f,
+				                  0.0f, -1.0f };
+
+			const float AT_[8] = { 1.0f, 1.0f,  1.0f,  0.0f,
+				                   0.0f, 1.0f, -1.0f, -1.0f };
+
+			const float B_[16] = { 1.0f, 0.0f,  0.0f,  0.0f,
+				                   0.0f, 1.0f, -1.0f,  1.0f,
+				                  -1.0f, 1.0f,  1.0f,  0.0f,
+				                   0.0f, 0.0f,  0.0f, -1.0f };
+
+			const float BT_[16] = { 1.0f,  0.0f, -1.0f,  0.0f,
+				                    0.0f,  1.0f,  1.0f,  0.0f,
+				                    0.0f, -1.0f,  1.0f,  0.0f,
+				                    0.0f,  1.0f,  0.0f, -1.0f };
+
+			const float G_[12] = { 1.0f,  0.0f,  0.0f,
+				                   0.5f,  0.5f,  0.5f,
+				                   0.5f, -0.5f,  0.5f,
+				                   0.0f,  0.0f,  1.0f };
+
+			const float GT_[12] = { 1.0f, 0.5f,  0.5f, 0.0f,
+				                    0.0f, 0.5f, -0.5f, 0.0f,
+				                    0.0f, 0.5f,  0.5f, 1.0f };
+
+			conv_winograd_cpu::conv_winograd_cpu(int input_Channel, int output_Channel, int kernelSize, int stride, int pad, bool bias_term, int device);
+
+			conv_winograd_cpu::conv_winograd_cpu(int input_Channel, int output_Channel, int kernelSize, int group, int stride, int pad, bool bias_term, int device);
+
+			virtual conv_winograd_cpu::~conv_winograd_cpu();
+
+			void Forward(const std::shared_ptr<tensor<float>>& bottom, std::shared_ptr<tensor<float>>& top) override;
+
+			void forward_bias(float* output, const float* bias) override;
+
+			void forward_gemm(const float* input, const float* weights, float* output, bool skip_im2col = false) override;
+
+			inline bool is_a_ge_zero_and_a_lt_b(int a, int b)
+			{
+				return static_cast<unsigned>(a) < static_cast<unsigned>(b);
+			}
+
+			inline void calculate_GgGT(const float *weight_data, float *u_data)
+			{
+				*(u_data + 0) = *(weight_data + 0);
+				*(u_data + 1) = (*(weight_data + 0) + *(weight_data + 1) + *(weight_data + 2)) / 2;
+				*(u_data + 2) = (*(weight_data + 0) - *(weight_data + 1) + *(weight_data + 2)) / 2;
+				*(u_data + 3) = *(weight_data + 2);
+				*(u_data + 4) = (*(weight_data + 0) + *(weight_data + 3) + *(weight_data + 6)) / 2;
+				*(u_data + 5) = (*(weight_data + 0) + *(weight_data + 1) + *(weight_data + 2) +
+					             *(weight_data + 3) + *(weight_data + 4) + *(weight_data + 5) +
+					             *(weight_data + 6) + *(weight_data + 7) + *(weight_data + 8)) / 4;
+				*(u_data + 6) = (*(weight_data + 0) + *(weight_data + 3) + *(weight_data + 6) -
+					             *(weight_data + 1) - *(weight_data + 4) - *(weight_data + 7) +
+					             *(weight_data + 2) + *(weight_data + 5) + *(weight_data + 8)) / 4;
+				*(u_data + 7) = (*(weight_data + 2) + *(weight_data + 5) + *(weight_data + 8)) / 2;
+				*(u_data + 8) = (*(weight_data + 0) - *(weight_data + 3) + *(weight_data + 6)) / 2;
+				*(u_data + 9) = (*(weight_data + 0) - *(weight_data + 3) + *(weight_data + 6) +
+					             *(weight_data + 1) - *(weight_data + 4) + *(weight_data + 7) +
+					             *(weight_data + 2) - *(weight_data + 5) + *(weight_data + 8)) / 4;
+				*(u_data + 10) = (*(weight_data + 0) - *(weight_data + 3) + *(weight_data + 6) -
+					              *(weight_data + 1) + *(weight_data + 4) - *(weight_data + 7) +
+					              *(weight_data + 2) - *(weight_data + 5) + *(weight_data + 8)) / 4;
+				*(u_data + 11) = (*(weight_data + 2) - *(weight_data + 5) + *(weight_data + 8)) / 2;
+				*(u_data + 12) = *(weight_data + 6);
+				*(u_data + 13) = (*(weight_data + 6) + *(weight_data + 7) + *(weight_data + 8)) / 2;
+				*(u_data + 14) = (*(weight_data + 6) - *(weight_data + 7) + *(weight_data + 8)) / 2;
+				*(u_data + 15) = *(weight_data + 8);
+			}
+
+			inline void calculate_BTdB(const float *tile_data, float *v_data)
+			{
+				*(v_data + 0) = *(tile_data + 0) - *(tile_data + 8) + *(tile_data + 10) - *(tile_data + 2);
+				*(v_data + 1) = *(tile_data + 1) - *(tile_data + 9) + *(tile_data + 2) - *(tile_data + 10);
+				*(v_data + 2) = *(tile_data + 9) - *(tile_data + 1) + *(tile_data + 2) - *(tile_data + 10);
+				*(v_data + 3) = *(tile_data + 1) - *(tile_data + 9) + *(tile_data + 11) - *(tile_data + 3);
+				*(v_data + 4) = *(tile_data + 4) + *(tile_data + 8) - *(tile_data + 6) - *(tile_data + 10);
+				*(v_data + 5) = *(tile_data + 5) + *(tile_data + 9) + *(tile_data + 6) + *(tile_data + 10);
+				*(v_data + 6) = *(tile_data + 6) + *(tile_data + 10) - *(tile_data + 5) - *(tile_data + 9);
+				*(v_data + 7) = *(tile_data + 5) + *(tile_data + 9) - *(tile_data + 7) - *(tile_data + 11);
+				*(v_data + 8) = *(tile_data + 8) - *(tile_data + 4) + *(tile_data + 6) - *(tile_data + 10);
+				*(v_data + 9) = *(tile_data + 9) - *(tile_data + 5) + *(tile_data + 10) - *(tile_data + 6);
+				*(v_data + 10) = *(tile_data + 5) - *(tile_data + 9) + *(tile_data + 10) - *(tile_data + 6);
+				*(v_data + 11) = *(tile_data + 9) - *(tile_data + 5) + *(tile_data + 7) - *(tile_data + 11);
+				*(v_data + 12) = *(tile_data + 4) - *(tile_data + 12) + *(tile_data + 14) - *(tile_data + 6);
+				*(v_data + 13) = *(tile_data + 5) - *(tile_data + 13) + *(tile_data + 6) - *(tile_data + 14);
+				*(v_data + 14) = *(tile_data + 13) - *(tile_data + 5) + *(tile_data + 6) - *(tile_data + 14);
+				*(v_data + 15) = *(tile_data + 5) - *(tile_data + 13) + *(tile_data + 15) - *(tile_data + 7);
+			}
+
+			inline void calculate_ATmA(const float *m_data, float *result)
+			{
+				*(result + 0) = *(m_data + 0) + *(m_data + 4) + *(m_data + 8) + *(m_data + 1) + *(m_data + 5) + *(m_data + 9) + *(m_data + 2) + *(m_data + 6) + *(m_data + 10);
+				*(result + 1) = *(m_data + 1) + *(m_data + 5) + *(m_data + 9) - *(m_data + 2) - *(m_data + 6) - *(m_data + 10) - *(m_data + 3) - *(m_data + 7) - *(m_data + 11);
+				*(result + 2) = *(m_data + 4) - *(m_data + 8) - *(m_data + 12) + *(m_data + 5) - *(m_data + 9) - *(m_data + 13) + *(m_data + 6) - *(m_data + 10) - *(m_data + 14);
+				*(result + 3) = *(m_data + 5) - *(m_data + 9) - *(m_data + 13) - *(m_data + 6) + *(m_data + 10) + *(m_data + 14) - *(m_data + 7) + *(m_data + 11) + *(m_data + 15);
+			}
+
+		};
+	}
+}
+
+
+#endif // !_CONV_WINOGRAD_CPU_HPP_
