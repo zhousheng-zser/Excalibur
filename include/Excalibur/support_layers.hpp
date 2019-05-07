de28ba0a8c3eea0afcca873edacdd80cf49371ba
fengye2two <fengye2two@163.com>
2019-05-07 16:16:39 +0800

    recover unicorn.cpp and support_layers

--- include/Excalibur/support_layers.hpp
+++ include/Excalibur/support_layers.hpp
@@ -19,7 +19,6 @@
 #include "mirrormax.hpp"
 #include "sigmoid.hpp"
 #include "deconv.hpp"
-#include "prune.hpp"
 
 #define Neuron_Name(name) private: \
 std::shared_ptr<tensor<float>> name##_top_data;\
