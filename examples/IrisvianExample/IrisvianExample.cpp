aaa7995b8061faee785dd3ceb2793265ed71f179
fengye2two <fengye2two@163.com>
2019-05-15 20:36:20 +0800

    tensor_operation simd support
    rgb2gray;
    preprocess;
    type_convert.

--- examples/IrisvianExample/IrisvianExample.cpp
+++ examples/IrisvianExample/IrisvianExample.cpp
@@ -1,7 +1,9 @@
 #include <fstream>
 #include <string>
 #include "../../include/Irisvian/IrisvianSearch.hpp"
+#include <glasssix/profiler.hpp>
 
+using namespace glasssix;
 using namespace glasssix::Irisvian;
 
 void static loadData(const char* filename, std::vector<const float*> &baseData, unsigned baseNumItem,
@@ -47,7 +49,7 @@ void static loadData(const char* filename, std::vector<const float*> &queryData,
 
 void search_first_time()
 {
-	std::string sourceDataPath = "../../data/map512.bin";
+	std::string sourceDataPath = "D:/projects/nsg/map512.bin";
 	std::vector<const float*> baseData;
 	std::vector<const float*> queryData;
 	const unsigned baseNum = 10000;
@@ -55,13 +57,35 @@ void search_first_time()
 	const unsigned data_dimension = 512;
 	loadData(sourceDataPath.c_str(), baseData, baseNum, queryData, queryNum, data_dimension);
 	IrisvianSearch irisvian(&baseData, data_dimension);
+
+	Profiler *profiler = Profiler::Get();
+	profiler->TurnON();
+	profiler->ScopeStart("kgraph");
+	for (int i = 0; i < 10; i++)
+	{
+		irisvian.buildGraph();
+	}
+	profiler->ScopeEnd();
+	profiler->TurnOFF();
+	profiler->DumpProfile("D:/kgraph_sequence.json");
+
 	irisvian.buildGraph();
 	irisvian.optimizeGraph();
 	const unsigned topK = 10;
 	std::vector<std::vector<unsigned>> returnIDs;
 	std::vector<std::vector<float>> returnSimilarities;
 	irisvian.searchVector(&queryData, topK, returnIDs, returnSimilarities);
-	irisvian.saveGraph("./search_index.graph", "./base_data.bin");
+	irisvian.saveGraph("D:/projects/nsg/search_index.graph");
+
+	std::cout << "result:" << std::endl;
+	for (int i = 0; i < returnIDs.size(); i++)
+	{
+		for (int j = 0; j < returnIDs[i].size(); j++)
+		{
+			std::cout << returnIDs[i][j] << " ," << returnSimilarities[i][j] << "   ";
+		}
+		std::cout << std::endl;
+	}
 }
 
 void search_second_time()
@@ -69,12 +93,12 @@ void search_second_time()
 	const unsigned queryNum = 10;
 	const unsigned data_dimension = 512;
 	IrisvianSearch irisvian(data_dimension);
-	irisvian.loadGraph("./search_index.graph", "./base_data.bin");
+	irisvian.loadGraph("D:/projects/nsg/search_index.graph", "D:/projects/nsg/map512.bin");
 	irisvian.optimizeGraph();
 	const unsigned topK = 10;
 	const std::vector<const float*>* baseData = irisvian.getBasedata();
 	std::vector<const float*> queryData;
-	loadData("../../data/map512.bin", queryData, queryNum, data_dimension);
+	loadData("D:/projects/nsg/map512.bin", queryData, queryNum, data_dimension);
 	std::vector<std::vector<unsigned>> returnIDs;
 	std::vector<std::vector<float>> returnDistancesInPercentage;
 	irisvian.searchVector(&queryData, topK, returnIDs, returnDistancesInPercentage);
