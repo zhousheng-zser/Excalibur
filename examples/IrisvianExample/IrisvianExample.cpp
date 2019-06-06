7fbde27a649bb3e470e2b5ae903a9cfc5e5a9a0a
fengye2two <fengye2two@163.com>
2019-06-06 14:05:13 +0800

    split unicorn and unirocn_mobile

--- examples/IrisvianExample/IrisvianExample.cpp
+++ examples/IrisvianExample/IrisvianExample.cpp
@@ -1,8 +1,10 @@
 #include <fstream>
 #include <string>
+#include <cfloat>
+#include <cmath>
+#include <climits>
 #include "../../include/Irisvian/IrisvianSearch.hpp"
-#include "../../include/Irisvian/distance.hpp"
-#include <glasssix/profiler.hpp>
+#include "../../include/Julius/simd_helper.hpp"
 #include <glasssix/timer.hpp>
 
 using namespace glasssix;
@@ -49,20 +51,64 @@ void static loadData(const char* filename, std::vector<const float*> &queryData,
 	in.close();
 }
 
-float inner(const float *benchmark, const float *m, int size)
+float calc_distance(const float *a, const float *b, int size)
 {
-	float res = 0.0f;
-	for (int q = 0; q < size; q++)
+#define AVX_DOT(addr1, addr2, dest, tmp1, tmp2) \
+		  tmp1 = _mm256_loadu_ps(addr1);\
+          tmp2 = _mm256_loadu_ps(addr2);\
+		  dest = mm_fmadd_ps(tmp1, tmp2, dest);
+
+	__m256 sum_aa = mm_setzero_ps();
+	__m256 sum_bb = mm_setzero_ps();
+	__m256 sum_ab = mm_setzero_ps();
+	__m256 l0, l1, l2, l3;
+	__m256 r0, r1, r2, r3;
+	unsigned D = (size + 7) & ~7U; // # dim aligned up to 256 bits, or 8 floats
+	unsigned DR = D % 32;
+	unsigned DD = D - DR;
+	const float *l = a;
+	const float *r = b;
+	const float *e_l = l + DD;
+	const float *e_r = r + DD;
+	switch (DR)
 	{
-		res += benchmark[q] * m[q];
+	case 24:
+		AVX_DOT(e_l + 16, e_l + 16, sum_aa, l2, l2);
+		AVX_DOT(e_r + 16, e_r + 16, sum_bb, r2, r2);
+		AVX_DOT(e_l + 16, e_r + 16, sum_ab, l2, r2);
+	case 16:
+		AVX_DOT(e_l + 8, e_l + 8, sum_aa, l1, l1);
+		AVX_DOT(e_r + 8, e_r + 8, sum_bb, r1, r1);
+		AVX_DOT(e_l + 8, e_r + 8, sum_ab, l1, r1);
+	case 8:
+		AVX_DOT(e_l, e_l, sum_aa, l0, l0);
+		AVX_DOT(e_r, e_r, sum_bb, r0, r0);
+		AVX_DOT(e_l, e_r, sum_ab, l0, r0);
+	}
+	for (unsigned i = 0; i < DD; i += 32, l += 32, r += 32)
+	{
+		AVX_DOT(l, l, sum_aa, l0, l0);
+		AVX_DOT(l + 8, l + 8, sum_aa, l1, l1);
+		AVX_DOT(l + 16, l + 16, sum_aa, l2, l2);
+		AVX_DOT(l + 24, l + 24, sum_aa, l3, l3);
+
+		AVX_DOT(r, r, sum_bb, r0, r0);
+		AVX_DOT(r + 8, r + 8, sum_bb, r1, r1);
+		AVX_DOT(r + 16, r + 16, sum_bb, r2, r2);
+		AVX_DOT(r + 24, r + 24, sum_bb, r3, r3);
+
+		AVX_DOT(l, r, sum_ab, l0, r0);
+		AVX_DOT(l + 8, r + 8, sum_ab, l1, r1);
+		AVX_DOT(l + 16, r + 16, sum_ab, l2, r2);
+		AVX_DOT(l + 24, r + 24, sum_ab, l3, r3);
 	}
 
-	return res;
-}
+	float res_aa = _mm256_sumall_ps(sum_aa);
+	float res_bb = _mm256_sumall_ps(sum_bb);
+	float res_ab = _mm256_sumall_ps(sum_ab);
 
-float calc_diff(const float *benchmark, const float *m, int size)
-{
-	return (inner(benchmark, m, size) / sqrt(inner(benchmark, benchmark, size) * inner(m, m, size)));
+	float result = res_ab / (sqrt(res_aa * res_bb));
+	return result;
 }
 
 void get_accurate_similarity(std::vector<const float*> &baseData, unsigned baseNumItem,
@@ -85,7 +131,7 @@ void get_accurate_similarity(std::vector<const float*> &baseData, unsigned baseN
 
 		for (int j = 0; j < baseNumItem; j++)
 		{
-			float distance = calc_diff(queryData[i], baseData[j], dim);
+			float distance = calc_distance(queryData[i], baseData[j], dim);
 
 			if (j == 0)
 			{
@@ -140,7 +186,7 @@ float get_accuracy(std::vector<std::vector<float>> &accurateSimilarities, std::v
 				++n_accurate;
 				++n_search;
 			}
-			else if (accurateSimilarities[i][n_accurate] < searchSimilarities[i][n_search]) {
+			else if (accurateSimilarities[i][n_accurate] > searchSimilarities[i][n_search]) {
 				++n_accurate;
 			}
 			else {
@@ -220,14 +266,71 @@ void search_second_time()
 	irisvian.searchVector(&queryData, topK, returnIDs, returnDistancesInPercentage);
 }
 
+void nsg_build_search()
+{
+	unsigned dim[18] = { 128,128,512,128,512,128,128,512,512,128,512,128,128,512,512,128,512,512 };
+	unsigned numItem[18] = { 1000,2000,1000,5000,2000,10000,20000,5000,10000,50000,20000,100000,200000,50000,100000,500000,200000,500000 };
 
+	glasssix::Timer calcTime;
+	double indexTime = 0, searchTime = 0;
+
+	std::ofstream out("./record.txt", std::fstream::out | std::fstream::trunc);
+
+	std::vector<const float*> baseData;
+	std::vector<const float*> queryData;
+
+	for (int i = 0; i < 18; ++i)
+	{
+		std::string sourceDataPath = "./map512.bin";
+		std::string nsgPath = "./nsg_" + std::to_string(dim[i]) + "_" + std::to_string(numItem[i]) + ".bin";
+		std::string searchResultPath = "./search_nsg_" + std::to_string(dim[i]) + "_" + std::to_string(numItem[i]) + ".bin";
+
+		baseData.clear();
+		queryData.clear();
+		unsigned baseNum = numItem[i];
+		unsigned queryNum = numItem[i] / 10;
+		unsigned dimension = dim[i];
+		loadData(sourceDataPath.c_str(), baseData, baseNum, queryData, queryNum, dimension);
+
+		calcTime.Start();
+		IrisvianSearch oIndex(&baseData, dimension);
+		oIndex.buildGraph();
+		calcTime.Stop();
+		indexTime = calcTime.GetElapsedMilliseconds() / 1000;
+
+		//oIndex.saveGraph(nsgPath.c_str());
+		//oIndex.loadGraph(nsgPath.c_str());
+
+		oIndex.optimizeGraph();
+		unsigned topK = 10;
+		std::vector<std::vector<unsigned>> returnIDs;
+		std::vector<std::vector<float>> returnSimilarities;
+		std::vector<std::vector<float>> accurateSimilarities;
+
+		calcTime.Start();
+		oIndex.searchVector(&queryData, topK, returnIDs, returnSimilarities);
+		calcTime.Stop();
+		searchTime = calcTime.GetElapsedMilliseconds() / queryNum;
+
+		get_accurate_similarity(baseData, baseNum, queryData, queryNum, dimension, topK, accurateSimilarities);
+		float accuracy = get_accuracy(accurateSimilarities, returnSimilarities, topK, queryNum);
+
+		out << "baseNum: " << baseNum << ", dimension: " << dimension << ", index: " << indexTime << ", search: " << searchTime << ", accuracy: " << accuracy << std::endl;
+		std::cout << "baseNum: " << baseNum << ", dimension: " << dimension << ", index: " << indexTime << ", search: " << searchTime << ", accuracy: " << accuracy << std::endl;
+		out.flush();
+
+		//oIndex.saveResult(searchResultPath.c_str(), returnIDs);
+	}
+}
 
 
 int main()
 {
-	search_first_time();
+	nsg_build_search();
+
+	//search_first_time();
 	//search_second_time();
 
-	//system("pause");
+	system("pause");
 	return 0;
 }
