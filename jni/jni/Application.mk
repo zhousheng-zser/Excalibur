755a485a7d8acdb1b56a0d72b3e3c0940c779d51
zhangyifu2016 <415699513@qq.com>
2019-07-15 19:02:02 +0800

    Add android jni interface

---
+++ jni/jni/Application.mk
@@ -0,0 +1,4 @@
+APP_ABI := arm64-v8a
+APP_CPPFLAGS += -fPIC -std=c++11 -fpermissive -frtti -fexceptions
+APP_STL := c++_static
+APP_PLATFORM := android-16
---
+++ jni/jni/Irisvian/Irisvian-jni.hpp
@@ -0,0 +1,22 @@
+#ifndef _IRISVIAN_JNI_HPP_
+#define _IRISVIAN_JNI_HPP_
+
+#include <jni.h>
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+JNIEXPORT void JNICALL Java_com_glasssix_Irisvian_Search_init__3_3F(JNIEnv *, jobject, jobjectArray);
+JNIEXPORT void JNICALL Java_com_glasssix_Irisvian_Search_init_I(JNIEnv *, jobject, jint);
+JNIEXPORT void JNICALL Java_com_glasssix_Irisvian_Search_finalize(JNIEnv *, jobject);
+JNIEXPORT void JNICALL Java_com_glasssix_Irisvian_Search_loadGraph_LJava_lang_String_2(JNIEnv *, jobject, jstring);
+JNIEXPORT void JNICALL Java_com_glasssix_Irisvian_Search_loadGraph_LJava_lang_String_2LJava_lang_String_2(JNIEnv *, jobject, jstring, jstring);
+JNIEXPORT void JNICALL Java_com_glasssix_Irisvian_Search_optimizeGraph(JNIEnv *, jobject);
+JNIEXPORT void JNICALL Java_com_glasssix_Irisvian_Search_searchVector(JNIEnv *, jobject, jobjectArray, jint, jobjectArray, jobjectArray);
+JNIEXPORT void JNICALL Java_com_glasssix_Irisvian_Search_saveResult(JNIEnv *, jobject, jstring, jobjectArray);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
