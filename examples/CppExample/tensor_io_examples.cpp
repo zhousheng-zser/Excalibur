d1626d9b4d2dd6338766d69608e27c0cd7e1a121
mingyu92 <mingyu92@126.com>
2019-07-24 20:17:32 +0800

    Examples.

--- examples/CppExample/tensor_io_examples.cpp
+++ examples/CppExample/tensor_io_examples.cpp
@@ -12,18 +12,38 @@ namespace glasssix
             static void loading_example()
             {
                 auto builder = tensor_builder_factory::create();
-                assert(builder->load_from(R"(E:\数据\DSC00052.JPG)"));
-                
+                assert(builder->load_from(R"(E:\数据\吔屎.jpg)"));
+
                 // By default, the parameters are NHWC, -1.
                 // Uncomment the lines below to set custom parameters.
                 // builder->tensor_parameters(NHWC);
                 // builder->tensor_parameters(NHWC, -1);
 
+                // Floating-point tensors.
                 auto rgb_float = builder->to_tensor_float(tensor_layout::rgb);
                 auto rgba_float = builder->to_tensor_float(tensor_layout::rgba);
                 auto grayscale_float = builder->to_tensor_float(tensor_layout::grayscale);
-                auto grayscale_3_float = builder->to_tensor_float(tensor_layout::grayscale_3);
 
+                // Uint8 tensors.
+                auto rgb_uint8 = builder->to_tensor_uint8(tensor_layout::rgb);
+                auto rgba_uint8 = builder->to_tensor_uint8(tensor_layout::rgba);
+                auto grayscale_uint8 = builder->to_tensor_uint8(tensor_layout::grayscale);
+
+                // Convert to grayscale and save it to the disk.
+                if (rgba_float)
+                {
+                    auto test = *rgba_float/* | tensor_convert_layout_to<tensor_layout::grayscale>*/;
+                    if (builder->from_tensor(test, tensor_layout::rgba))
+                    {
+                        builder->save_to(R"(F:\哈哈哈.png)");
+                    }
+                }
+
+                // Change the underlying type.
+                if (rgb_uint8)
+                {
+                    auto rgb_change = *rgb_uint8 | tensor_convert_to<float>;
+                }
             }
         };
     }
