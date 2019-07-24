5507447f98a971ce3b8350e331c364aedfe38fd1
mingyu92 <mingyu92@126.com>
2019-07-24 20:13:14 +0800

    Extended FreeImage

--- include/Infrastructure/fi_extended_conversions.hpp
+++ include/Infrastructure/fi_extended_conversions.hpp
@@ -2,6 +2,7 @@
 
 #include <tuple>
 #include <cstdint>
+#include <functional>
 #include <unordered_map>
 
 struct FIBITMAP;
@@ -24,15 +25,31 @@ namespace glasssix
             static FIBITMAP* rgbf_to_standard_type(FIBITMAP* bitmap);
 
             /// <summary>
+            /// Convert a RGBAF image to a standard type.
+            /// </summary>
+            /// <param name="bitmap">The bitmap</param>
+            /// <returns>The result</returns>
+            static FIBITMAP* rgbaf_to_standard_type(FIBITMAP* bitmap);
+
+            /// <summary>
+            /// Convert an image to a standard type.
+            /// </summary>
+            /// <param name="bitmap">The bitmap</param>
+            /// <returns>The result</returns>
+            static FIBITMAP* to_standard_type(FIBITMAP* bitmap);
+
+            /// <summary>
             /// Get the channel size of an image type.
             /// </summary>
             /// <param name="type">The image type</param>
             /// <returns>The channel size</returns>
-            static size_t channel_bytes(int type);
+            static size_t channel_bytes_of(int type);
         private:
-            static std::tuple<uint8_t*, int, int, int> get_parameters_core(FIBITMAP* bitmap);
+            static std::tuple<uint8_t*, int, int, int, int, int> get_parameters_core(FIBITMAP* bitmap);
+            static FIBITMAP* to_standard_type_core(FIBITMAP* bitmap, const std::function<void(uint8_t*, uint8_t*)>& pixel_handler);
         private:
-            static const std::unordered_map<int, size_t> channel_byte_mapping_;
+            static constexpr int uint8_bits_ = 8;
+            static const std::unordered_map<int, size_t> channel_bytes_mapping_;
         };
     }
 }
