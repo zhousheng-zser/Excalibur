3bfb5847a9f586da81ef91b4f4eb65f014debcaa
mingyu92 <mingyu92@126.com>
2019-07-25 15:21:38 +0800

    Merge License: Capacity of serialized authorization, to avert  threading conflicts.

--- src/License/license_wrapper.cpp
+++ src/License/license_wrapper.cpp
@@ -3,45 +3,32 @@
 #include "common.hpp"
 #include "watchdog.hpp"
 #include "license_wrapper.h"
-#include "windows_com_global.hpp"
+//#include "windows_com_global.hpp"
 
-#include <mutex>
 #include <string>
+
+#ifdef _MSC_VER
+#include <Windows.h>
+#endif
 
 namespace glasssix
 {
-	namespace hippogriff
-	{
-		bool has_updated_license = false;
+    namespace hippogriff
+    {
+        extern unsigned long license_thread_id;
 
-		/// <summary>
-		/// Check if a license is valid.
-		/// </summary>
-		/// <param name="compoent_name">The component name</param>
-		/// <param name="error_code">The error code</param>
-		/// <returns>The validation result</returns>
-		bool check_license_impl(const std::string& component_name, license_error_code& error_code, bool readonly)
-		{
-			static std::mutex lock;
-			std::lock_guard<std::mutex> locker{ lock };
-
-			error_code = license_error_code::none;
-
-			try
-			{
-				windows_com_global::instance();
-				license_context context{ component_name };
-				context.check(readonly);
-
-				return true;
-			}
-			catch (license_error& error)
-			{
-				error_code = error.code();
-
-				return false;
-			}
-		}
+        /// <summary>
+        /// Check if a license is valid and terminate the process when failed.
+        /// </summary>
+        void check_license_fatal_exit_sync()
+        {
+            if (license_thread_id != 0)
+            {
+#ifdef _MSC_VER
+                PostThreadMessage(license_thread_id, WM_ACTIVATE, 0, 0);
+#endif
+            }
+        }
 
         /// <summary>
         /// Check if a license is valid and terminate the process when failed.
@@ -49,14 +36,10 @@ namespace glasssix
         /// <param name="component_name">The component name</param>
         void check_license_fatal_exit(const std::string& component_name)
         {
-			license_error_code error_code;
-
-			if (!check_license_impl(component_name, error_code, has_updated_license))
-			{
-				common::fatal_exit();
-			}
-
-			has_updated_license = true;
+            if (!check_license(component_name))
+            {
+                common::fatal_exit();
+            }
         }
 
         /// <summary>
@@ -71,23 +54,38 @@ namespace glasssix
             return check_license(component_name, error_code);
         }
 
-		/// <summary>
-		/// Check if a license is valid.
-		/// </summary>
-		/// <param name="compoent_name">The component name</param>
-		/// <param name="error_code">The error code</param>
-		/// <returns>The validation result</returns>
-		bool check_license(const std::string& component_name, license_error_code& error_code)
-		{
-			return check_license_impl(component_name, error_code, false);
-		}
+        /// <summary>
+        /// Check if a license is valid.
+        /// </summary>
+        /// <param name="compoent_name">The component name</param>
+        /// <param name="error_code">The error code</param>
+        /// <returns>The validation result</returns>
+        bool check_license(const std::string& component_name, license_error_code& error_code)
+        {
+            error_code = license_error_code::none;
 
-		/// <summary>
-		/// Start the watchdog timer.
-		/// </summary>
-		void start_watchdog(const std::string& component_name)
-		{
-			watchdog::instance().start(component_name);
-		}
-	}
+            try
+            {
+                //windows_com_global::instance();
+                license_context context{ component_name };
+                context.check();
+
+                return true;
+            }
+            catch (license_error& error)
+            {
+                error_code = error.code();
+
+                return false;
+            }
+        }
+
+        /// <summary>
+        /// Start the watchdog timer.
+        /// </summary>
+        void start_watchdog(const std::string& component_name)
+        {
+            watchdog::instance().start(component_name);
+        }
+    }
 }
