af2ca332cfd9a70f19fb163917ddd8d8e8641fe0
mingyu92 <mingyu92@126.com>
2020-03-30 10:29:36 +0800

    Interface Adapter

--- include/Irisviel/irisviel_c.h
+++ include/Irisviel/irisviel_c.h
@@ -14,28 +14,31 @@ namespace glasssix
 	}
 }
 
-extern "C" IRISVIEL_C_EXPORT glasssix::irisviel::face_service *Irisviel_NewInstance(int max_items, int dimension, char * working_directory);
+extern "C"
+{
+	IRISVIEL_C_EXPORT glasssix::irisviel::face_service * irisviel_new_instance(int max_items, int dimension, const char* working_directory);
 
-extern "C" IRISVIEL_C_EXPORT void Irisviel_ReleaseInstance(glasssix::irisviel::face_service *instance);
+	IRISVIEL_C_EXPORT void irisviel_release_instance(glasssix::irisviel::face_service* instance);
 
-extern "C" IRISVIEL_C_EXPORT char *Irisviel_save_path(glasssix::irisviel::face_service *instance);
+	IRISVIEL_C_EXPORT char* irisviel_database_path(glasssix::irisviel::face_service* instance);
 
-extern "C" IRISVIEL_C_EXPORT char *Irisviel_tmp_path(glasssix::irisviel::face_service *instance);
+	IRISVIEL_C_EXPORT char* irisviel_cache_path(glasssix::irisviel::face_service * instance);
 
-extern "C" IRISVIEL_C_EXPORT void Irisviel_load_databases(glasssix::irisviel::face_service *instance);
+	IRISVIEL_C_EXPORT void irisviel_load_databases(glasssix::irisviel::face_service * instance);
 
-extern "C" IRISVIEL_C_EXPORT int Irisviel_search(glasssix::irisviel::face_service *instance, glasssix::irisviel::database_search_result **result, float *feature, int top);
+	IRISVIEL_C_EXPORT int irisviel_search(glasssix::irisviel::face_service * instance, glasssix::irisviel::database_search_result * *result, float* feature, int top);
 
-extern "C" IRISVIEL_C_EXPORT void Irisviel_remove_all(glasssix::irisviel::face_service *instance);
+	IRISVIEL_C_EXPORT void irisviel_remove_all(glasssix::irisviel::face_service * instance);
 
-extern "C" IRISVIEL_C_EXPORT void Irisviel_delete_features(glasssix::irisviel::face_service *instance, int *needs_delete_files_num, char **needs_delete_files[], int keys_num, char *keys[]);
+	IRISVIEL_C_EXPORT void Irisviel_delete_features(glasssix::irisviel::face_service * instance, int* needs_delete_files_num, char** needs_delete_files[], int keys_num, char* keys[]);
 
-extern "C" IRISVIEL_C_EXPORT void Irisviel_delete_feature(glasssix::irisviel::face_service *instance, int *needs_delete_files_num, char **needs_delete_files[], char *key);
+	IRISVIEL_C_EXPORT void Irisviel_delete_feature(glasssix::irisviel::face_service * instance, int* needs_delete_files_num, char** needs_delete_files[], char* key);
 
-extern "C" IRISVIEL_C_EXPORT void Irisviel_add_features(glasssix::irisviel::face_service *instance, int data_num, glasssix::irisviel::database_record *data);
+	IRISVIEL_C_EXPORT void Irisviel_add_features(glasssix::irisviel::face_service * instance, int data_num, glasssix::irisviel::database_record * data);
 
-extern "C" IRISVIEL_C_EXPORT void Irisviel_add_feature(glasssix::irisviel::face_service *instance, glasssix::irisviel::database_record &data);
+	IRISVIEL_C_EXPORT void Irisviel_add_feature(glasssix::irisviel::face_service * instance, glasssix::irisviel::database_record & data);
 
-extern "C" IRISVIEL_C_EXPORT void Irisviel_update_feature(glasssix::irisviel::face_service *instance, glasssix::irisviel::database_record &data);
+	IRISVIEL_C_EXPORT void Irisviel_update_feature(glasssix::irisviel::face_service * instance, glasssix::irisviel::database_record & data);
 
-extern "C" IRISVIEL_C_EXPORT void Irisviel_update_more(glasssix::irisviel::face_service *instance, int data_num, glasssix::irisviel::database_record *data);
+	IRISVIEL_C_EXPORT void Irisviel_update_more(glasssix::irisviel::face_service * instance, int data_num, glasssix::irisviel::database_record * data);
+}
