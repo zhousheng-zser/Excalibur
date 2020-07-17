a2795f828fe68ae16289675a566a5bd6372bae9c
Usami Saori <inlmouse@hotmail.com>
2020-07-17 12:08:52 +0800

    Add: relu CUDA support

--- src/Excalibur/operation_relu.cpp
+++ src/Excalibur/operation_relu.cpp
@@ -80,20 +80,9 @@ namespace glasssix
 			}
 		}
 
-
-//		template<typename Dtype>
-//		void operation_relu<Dtype>::forward_gpu_f32(
-//#ifdef USE_CUDA
-//			cublasHandle_t &cublas_handle_,
-//#ifdef USE_CUDNN
-//			cudnnHandle_t cudnn_handle,
-//#endif //!USE_CUDNN
-//#endif //!USE_CUDA
-//			const std::vector<std::shared_ptr<memory::tensor<float>>>& bottoms,
-//			std::vector<std::shared_ptr<memory::tensor<float>>>& tops)
-//		{
-//			NOT_IMPLEMENTED;
-//		}
+#ifndef USE_CUDA
+		STUB_GPU(operation_relu);
+#endif
 
 		INSTANCE_CLASS(operation_relu);
 		REGISTE(operation_relu);
