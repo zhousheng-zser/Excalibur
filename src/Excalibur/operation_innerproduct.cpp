bb4ed3d9759ca8a3f5911a090db3cb0ac6785c6f
zhangyifu2016 <415699513@qq.com>
2021-06-16 16:50:25 +0800

    fix: move _mm256_reduce_add_ps function to operation_innerproduct.cpp

--- src/Excalibur/operation_innerproduct.cpp
+++ src/Excalibur/operation_innerproduct.cpp
@@ -9,6 +9,19 @@ namespace glasssix
 {
     namespace excalibur
     {
+#if(SIMD_X86_INSTR_SET >= SIMD_X86_AVX_VERSION) && (SIMD_X86_INSTR_SET <= SIMD_X86_AVX2_VERSION)
+        static inline float _mm256_reduce_add_ps(__m256 x)
+        {
+            /* ( x3+x7, x2+x6, x1+x5, x0+x4 ) */
+            const __m128 x128 = _mm_add_ps(_mm256_extractf128_ps(x, 1), _mm256_castps256_ps128(x));
+            /* ( -, -, x1+x3+x5+x7, x0+x2+x4+x6 ) */
+            const __m128 x64 = _mm_add_ps(x128, _mm_movehl_ps(x128, x128));
+            /* ( -, -, -, x0+x1+x2+x3+x4+x5+x6+x7 ) */
+            const __m128 x32 = _mm_add_ss(x64, _mm_shuffle_ps(x64, x64, 0x55));
+            /* Conversion to float is a no-op on x86-64 */
+            return _mm_cvtss_f32(x32);
+        }
+#endif
 #if (SIMD_X86_INSTR_SET >= SIMD_X86_SSE_VERSION)
         static inline float _mm_reduce_add_ps(__m128 x128)
         {
