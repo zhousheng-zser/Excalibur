c77ddb0ae67130f46c7c1720d751cc96cda20ab3
zhangyifu2016 <415699513@qq.com>
2021-08-25 18:38:26 +0800

    fix: Add cuda version requirement and gencode setting

--- cmake/Dependencies.cmake
+++ cmake/Dependencies.cmake
@@ -2,6 +2,9 @@ find_package(Threads REQUIRED)
 
 if(USE_CUDA)
 	find_package(CUDA REQUIRED)
+	if(${CUDA_VERSION} VERSION_LESS 11.1)
+		message(FATAL_ERROR "CUDA version is too lower(${CUDA_VERSION} vs 11.1)")
+	endif()
 endif()
 
 if(USE_OPENMP)
