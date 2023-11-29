05574322a4a39e97b541d3d7640706fdc70330da
张义夫 <zhangyifu@glasssix.com>
2023-11-29 14:56:58 +0800

    refactor: shufflechannel forward_gpu_f32 code

--- include/Excalibur/operation_shufflechannel.hpp
+++ include/Excalibur/operation_shufflechannel.hpp
@@ -22,15 +22,15 @@ namespace glasssix
 			virtual void forward_cpu_f32(const std::vector<std::shared_ptr<memory::tensor<float>>>& bottoms,
 				std::vector<std::shared_ptr<memory::tensor<float>>>& tops);
 
-			virtual void forward_gpu_f32(
 #ifdef USE_CUDA
+			virtual void forward_gpu_f32(
 				cublasHandle_t& cublas_handle_,
 #ifdef USE_CUDNN
 				cudnnHandle_t cudnn_handle,
 #endif //!USE_CUDNN
-#endif //!USE_CUDA
 				const std::vector<std::shared_ptr<memory::tensor<float>>>& bottoms,
 				std::vector<std::shared_ptr<memory::tensor<float>>>& tops);
+#endif //!USE_CUDA
 
 		private:
 			int group_ = 1;
